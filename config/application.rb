e5fb0c3cebe9b5bcf334d315eafe90f83fa36ec5
Sourabh Ghorpade <sourabh.ghorpade@gmail.com>
2023-04-23 10:15:26 +0530

    Set Sidekiq as the ActiveJob adapter

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,8 @@ module PaymentProcessor
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Use Sidekiq as the ActiveJob adapter
+    config.active_job.queue_adapter = :sidekiq
   end
 end
